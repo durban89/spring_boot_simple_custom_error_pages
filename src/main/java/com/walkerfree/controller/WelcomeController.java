ccfbaa1fb3d6c635a9529ca6e60a6cd1363b193e
durban.zhang <durban.zhang@gmail.com>
2018-05-18 21:05:17 +0800

    注释不需要的
    Change-Id: I662b35884070acf5055519eeaf99008a5781bfa7
    Signed-off-by: durban.zhang <durban.zhang@gmail.com>

--- src/main/java/com/walkerfree/controller/WelcomeController.java
+++ src/main/java/com/walkerfree/controller/WelcomeController.java
@@ -3,7 +3,7 @@ package com.walkerfree.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
